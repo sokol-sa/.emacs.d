0d9483af5decae25ea930f13f6ba1f6023ca6bc3
Serhii Sokil <serhii@Serhiis-MacBook-Air.local>
2025-11-08 19:12:44 +0200

    Add packages-archives and they priorities

--- init.el
+++ init.el
@@ -28,10 +28,20 @@
 
 (global-set-key "\C-x\ \C-g" 'recentf-open-files)	;; Set keybindings for open menu recent files
 
-;; MELPA initialization
+;; Packages initialization
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+
+;; Set priorities of archives
+(setopt package-archive-priorities
+        '(("gnu" . 40)
+          ("nongnu" . 30)
+          ("melpa-stable" . 20)
+          ("melpa" . 10)))
+
 (package-initialize)
 
 (unless package-archive-contents
