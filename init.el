2f60f5524bed3d4c0f7b1d9703790c1d2c6ffc81
Serhii Sokil <serhii@Serhiis-MacBook-Air.local>
2025-11-13 12:31:04 +0200

    Rewrite some initialization with rules on version Emacs >28.1

--- init.el
+++ init.el
@@ -1,22 +1,48 @@
-(menu-bar-mode -1)									;; Hide menu on top
-(tool-bar-mode -1)									;; Hide bar icon on top
-(tab-bar-mode t)									;; Enable tab's on top
-(setq inhibit-splash-screen t)						;; Disable start screen EMACS
-(if (display-graphic-p)
-    (scroll-bar-mode -1))							;; Hide scrollbar in GUI mode
-(setq-default initial-scratch-message nil)			;; Disable message on top SCRATCH buffer
-(setq use-file-dialog nil)							;; Ask for textual confirmation instead of GUI
-(setq make-backup-files nil)						;; Don't make backup file
-(setq auto-save-list-file-name nil)					;; Don't make autosave list
-(setq auto-save-default nil)						;; Diasable default autosave files buffer
+;; Start initialization
+(custom-set-variables
+ '(menu-bar-mode nil "Hide menu on top")
+ '(tab-bar-mode t "Enable tab's on top")
+ '(use-short-answers t "y and n for answers")
+ '(inhibit-splash-screen t "Disable start screen EMACS")
+ '(initial-scratch-message nil "Disable message on top SCRATCH buffer")
+ '(use-file-dialog nil "Ask for textual confirmation instead of GUI")
+ '(make-backup-files nil "Don't make backup file")
+ '(auto-save-list-file-name nil "Don't make autosave file list")
+ '(auto-save-default nil "Disable default autosave files buffer")
+ '(recentf-mode t "Enable mode of recent files")
+ '(ido-mode #'both "Enable extra open file mode")
+ '(save-place-mode t "Enable restore cursor position in file")
+ '(delete-selection-mode t "Enable delete marked text with typing or DEL, BACKSPACE")
+ '(ring-bell-function #'ignore "Shut up the bell"))
 
-(recentf-mode t)									;; Enable menu of recent files
-(ido-mode 1)										;; Enable extra open file mode
+(when (fboundp 'tool-bar-mode)						;; If EMACS run in GUI mode
+  (custom-set-variables
+   '(tool-bar-mode nil "Disable bar icon on top")
+   '(scroll-bar-mode nil "Disable scrollbar")))
 
-(global-set-key (kbd "TAB") 'self-insert-command)	;; Enable insert indent with TAB-key
+;;(menu-bar-mode -1)									;; Hide menu on top
+;;(tab-bar-mode t)									;; Enable tab's on top
+;;(setq inhibit-splash-screen t)						;; Disable start screen EMACS
+;;(if (display-graphic-p)
+;;    (scroll-bar-mode -1))							;; Hide scrollbar in GUI mode
+;;(tool-bar-mode -1)									;; Hide bar icon on top
+
+;;(setq-default initial-scratch-message nil)			;; Disable message on top SCRATCH buffer
+;; (setq use-file-dialog nil)							;; Ask for textual confirmation instead of GUI
+;;(setq make-backup-files nil)						;; Don't make backup file
+;;(setq auto-save-list-file-name nil)					;; Don't make autosave list
+;;(setq auto-save-default nil)						;; Diasable default autosave files buffer
+
+;;(recentf-mode t)									;; Enable menu of recent files
+
+;;(ido-mode 1)										;; Enable extra open file mode
+
+;;(defalias 'yes-or-no-p 'y-or-n-p)					;; Use y and n for answer
+
+;;(setq ring-bell-function 'ignore)					;; shut up the bell
+
+(keymap-global-set "TAB" 'self-insert-command)	;; Enable insert indent with TAB-key
 (setq-default tab-width 4)							;; Set TAB indent in 4 symbols
-(setq ring-bell-function 'ignore)					;; shut up the bell
-(defalias 'yes-or-no-p 'y-or-n-p)					;; Use y and n for answer
 
 ;; Define function for align all comments
 (defun align-comments ()
@@ -24,9 +50,9 @@
   (interactive)															
   (align-regexp (region-beginning) (region-end)							
                 (concat "\\(\\s-*\\)" (regexp-quote comment-start))))	
-(global-set-key (kbd "C-c a c") 'align-comments)	;; Set keybindings for align-comments
+(keymap-global-set "C-c a c" 'align-comments)	;; Set keybindings for align-comments
 
-(global-set-key "\C-x\ \C-g" 'recentf-open-files)	;; Set keybindings for open menu recent files
+(keymap-global-set "\C-x\ \C-g" 'recentf-open-files)	;; Set keybindings for open menu recent files
 
 ;; Packages initialization
 (require 'package)
@@ -34,17 +60,20 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(package-initialize)
 
 ;; Set priorities of archives
-(setopt package-archive-priorities
+(custom-set-variables)
+	'(package-archive-priorities
         '(("gnu" . 40)
           ("nongnu" . 30)
           ("melpa-stable" . 20)
           ("melpa" . 10)))
-
-(package-initialize)
+ '(package-native-compile t "Compile packages from install but not first start")
+ '(package-user-dir init-emacs-package-user-dir)
 
 (unless package-archive-contents
+  (message "Refresh list archives")
   (package-refresh-contents))						;; Refresh packages list if it not present
 
 ;;(unless (package-installed-p 'use-package)			;; Uncomment this if needed oldest version EMACS <30
@@ -56,6 +85,21 @@
 ;; Install needed packages
 (dolist (package-need-install '(atom-one-dark-theme company magit racket-mode slime tramp windsize xterm-color))
   (eval `(use-package ,package-need-install)))
+
+;; Builtin package. Save and restore EMACS state between session
+(use-package desktop
+	:custom
+	(desktop-auto-save-timeout 20 "Автосохранение каждые 20 секунд")
+	(desktop-load-locked-desktop t "Загрузка desktop-файла даже если он заблокирован")
+	(desktop-restore-frames t "Восстанавливать состояние фреймов")
+	(desktop-save t "Сохранять состояние рабочего стола без лишних вопросов")
+	:config
+	;; Modes buffersnot need save and restore
+	(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+	(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+	(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+	;; Switch mode to enable
+	(desktop-save-mode 1))
 
 (global-company-mode)								;; Enable global mode autocomplete
 
